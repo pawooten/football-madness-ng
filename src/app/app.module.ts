cf86fa40f6c11780aa2b2990d00e8974b1478516
Paul Wooten <Paul.Wooten@hyland.com>
2021-04-08 21:17:37 -0400

    style, layout message window

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PlayFieldCanvasComponent } from './play-field-canvas/play-field-canvas.component';
+import { MessageWindowComponent } from './message-window/message-window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayFieldCanvasComponent
+    PlayFieldCanvasComponent,
+    MessageWindowComponent
   ],
   imports: [
     BrowserModule
