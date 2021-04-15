68fb7300a2894e18e5461100625872f00de34626
Paul Wooten <Paul.Wooten@hyland.com>
2021-04-14 20:50:47 -0400

    added components directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PlayFieldCanvasComponent } from './play-field-canvas/play-field-canvas.component';
-import { MessageWindowComponent } from './message-window/message-window.component';
+import { PlayFieldCanvasComponent } from './components/play-field-canvas/play-field-canvas.component';
+import { MessageWindowComponent } from './components/message-window/message-window.component';
 
 @NgModule({
   declarations: [
