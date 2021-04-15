d8903acda2656872906061c94c7b63ab85630e8c
Paul Wooten <Paul.Wooten@hyland.com>
2021-04-14 21:24:52 -0400

    flashing message window

--- src/app/components/play-field-canvas/play-field-canvas.component.ts
+++ src/app/components/play-field-canvas/play-field-canvas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../../services/message-service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-play-field-canvas',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayFieldCanvasComponent implements OnInit {
 
-  constructor() { }
+  private showMessageWindowSubject = new BehaviorSubject<boolean>(false);
+
+  readonly showMessageWindow$ = this.showMessageWindowSubject.asObservable();
+
+  private tick = 0;
+
+  constructor(private messageService: MessageService) {
+    const interval = setInterval(() => {
+      this.tick += 1;
+      console.log(this.tick);
+      const showWindow = this.tick % 2 == 0;
+      this.showMessageWindowSubject.next(showWindow);
+      if (this.tick > 6) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 
   ngOnInit() {
   }
